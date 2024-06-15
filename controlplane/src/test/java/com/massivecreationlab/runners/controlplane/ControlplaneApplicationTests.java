34915e986eee45f8aa721130298655fa2a795d3f
themassiveone <business.daniel.mass@gmail.com>
2024-06-15 09:04:01 +0200

    feat: dockerfile correctly set up

--- controlplane/src/test/java/com/massivecreationlab/runners/controlplane/ControlplaneApplicationTests.java
+++ controlplane/src/test/java/com/massivecreationlab/runners/controlplane/ControlplaneApplicationTests.java
@@ -2,8 +2,10 @@ package com.massivecreationlab.runners.controlplane;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootTest
+@ComponentScan(basePackages = "com.massivecreationlab.runners.controlplane")
 class ControlplaneApplicationTests {
 
     @Test
